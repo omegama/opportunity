463dea1e252dc9c4971626e88ea1ce4a6e0391c1
YumeVamp <34081202+YumeVamp@users.noreply.github.com>
2018-09-26 17:53:07 -0500

    Update conROS.ino
    Update con impresion de strings dependiendo el valor de humedad

--- conROS.ino
+++ conROS.ino
@@ -1,43 +1,47 @@
 #include <ros.h>
 #include <std_msgs/String.h>
-
+#include <std_msgs/Int16.h>
 ros::NodeHandle nh;
 
 std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
+std_msgs::Int16 hum_value;
 
+ros::Publisher pub_hum("hum_string", &hum_msg);
+ros::Publisher pub_mi_variable("hum_value", &hum_value);
 void setup() { 
     nh.initNode();
-    nh.advertise(chatter);
+    nh.advertise(pub_hum);
+    nh.advertise(pub_mi_variable);
     Serial.begin(9600);
-    pinMode(7, INPUT);//Digital
 }
  
 void loop() 
 {
    int lecturaDeHumedad = analogRead(A0);//Analogo
-   Serial.print(lecturaDeHumedad);
-  //humedad entre 1000 y 1023 esta super seco
+   hum_value.data = lecturaDeHumedad;
+  
    if(lecturaDeHumedad>=1000)
    {
-      chatter.publish("Fuera del suelo");
-      //Serial.println("Fuera del suelo");     
+      hum_msg.data="Fuera del suelo";
+      pub_hum.publish(&hum_msg);     
    }
    else if(lecturaDeHumedad<1000 && lecturaDeHumedad>=600)
    {
-      chatter.publish("Esta seco el suelo");
-      //Serial.println("Esta seco el suelo");   
+      hum_msg.data="Esta seco el suelo";
+      pub_hum.publish(&hum_msg);   
    }
    else if(lecturaDeHumedad<600 && lecturaDeHumedad>=370)
    {
-      chatter.publish("Esta humedo");
-      //Serial.println("Esta humedo");
+      hum_msg.data="Esta humedo";
+      pub_hum.publish(&hum_msg);
    }
    else if(lecturaDeHumedad<370)
    {
-      chatter.publish("Esta empapado");
-      //Serial.println("Esta empapado");
+       hum_msg.data="Esta empapado");
+       pub_hum.publish(&hum_msg);
    }
-   delay(1000);
+   
+    pub_mi_variable.publish(&hum_value);
+    delay(1000);
     nh.spinOnce();
 }
