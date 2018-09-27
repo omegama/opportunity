3afaa73f161fc4b685174844d0290ee3344640f0
YumeVamp <34081202+YumeVamp@users.noreply.github.com>
2018-09-27 12:32:28 -0500

    Update Servos.ino

--- Servos/Servos.ino
+++ Servos/Servos.ino
@@ -6,57 +6,59 @@
 
 #include <Servo.h> 
 #include <ros.h>
-#include <std_msgs/UInt16.h>
 #include <geometry_msgs/Quaternion.h>
 
 ros::NodeHandle  nh;
 
 Servo servo1;
 Servo servo2;
-int prueba=1;
+//int prueba=1;
+int anguloA=0;
+int anguloB=0;
+float ser2R=0;
+float ser2B=0;
+float ser1R=0;
+float ser1B=0;
+int angle_increment=5;
+
 void servo_cbA(const geometry_msgs::Quaternion& servos_msg){
-float ser2R=servos_msg.x;
-float ser2B=servos_msg.y;
-float ser1R=servos_msg.z;
-float ser1B=servos_msg.w;
+float ser2R=servos_msg.x; //servo de arriba da para arriba
+float ser2B=servos_msg.y; //servo de arriba da para abajo
+float ser1R=servos_msg.z; //servo de abajo da para arriba
+float ser1B=servos_msg.w; //servo de abajo da para abajo
 //while ser1A!=0
 
 //void servo_cbA( const std_msgs::UInt16& servoA_msg){
   int anguloA=0;
   int anguloB=0;
 //  int x = servoA_msg;
-  while(true)
-  {
+  
   if(ser2R==1)
   {
-    anguloA+=10;
+    anguloA+=angle_increment;
     servo1.write(anguloA);
     //delay(500);
   }
   if(ser2B==1)
   {
-    anguloA-=10;
+    anguloA-=angle_increment;
     servo1.write(anguloA);
     //delay(500);
   }
   if(ser1R==1)
   {
-    anguloB+=10;
+    anguloB+=angle_increment;
     servo2.write(anguloB);
     //delay(500);
   }
   if(ser1B==1)
   {
-    anguloB-=10;
+    anguloB-=angle_increment;
     servo2.write(anguloB);
     //delay(500);
   }
-  }
   
   
-  
-   //set servo angle, should be from 0-180  
-  //digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
 }
 
 ros::Subscriber<geometry_msgs::Quaternion> sub("servos", servo_cbA);
@@ -69,7 +71,7 @@ void setup(){
   nh.initNode();
   nh.subscribe(sub);
   
-  servo1.attach(8); //attach it to pin 9
+  servo1.attach(10); //attach it to pin 9
   servo2.attach(9);
 }
 
